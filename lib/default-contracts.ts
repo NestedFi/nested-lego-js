0add41334e9f457adb8862d65956d1975fdc6f61
Olivier Guimbal <olivier.guimbal@gadz.org>
2022-01-17 15:34:14 +0100

    Upgrade contracts

--- lib/default-contracts.ts
+++ lib/default-contracts.ts
@@ -20,19 +20,19 @@ export const defaultContracts: { [key in keyof typeof Chain]: ConnectionConfig }
     },
     bsc: {
         providerConfig: 'https://bsc-dataseed.binance.org/',
-        factoryAddress: '0xBd847b0BD0f78F46176dBAB7b187Ce2934ad6171',
+        factoryAddress: '0x1dB81116467789b7dCC3B070ee8f5aA4d90D6940',
         wrappedToken: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
         chainId: 56,
     },
     avax: {
         providerConfig: 'https://api.avax.network/ext/bc/C/rpc',
-        factoryAddress: '0xBd847b0BD0f78F46176dBAB7b187Ce2934ad6171',
+        factoryAddress: '0x1dB81116467789b7dCC3B070ee8f5aA4d90D6940',
         wrappedToken: '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7',
         chainId: 43114,
     },
     poly: {
         providerConfig: 'https://polygon-rpc.com',
-        factoryAddress: '0x74aFADa131db545dF782D196C8736D97593fd330',
+        factoryAddress: '0xD6D813e31558b45769B83E33Fe10cDef76128Ffc',
         wrappedToken: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
         chainId: 137,
     },
