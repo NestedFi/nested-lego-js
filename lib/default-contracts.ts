1453dd17c84af09299098f8e2c89a5a20f5fe8e3
adrien <adrien.supizet@gmail.com>
2022-04-02 12:07:14 +0200

    wip add support for ethereum

--- lib/default-contracts.ts
+++ lib/default-contracts.ts
@@ -13,10 +13,10 @@ export const FIXED_FEE = 0.01;
 /** Last version of the nested factory that has been deployed */
 export const defaultContracts: { [key in keyof typeof Chain]: ConnectionConfig } = {
     eth: {
-        providerConfig: 'homestead',
-        factoryAddress: null,
-        wrappedToken: null,
-        chainId: null,
+        providerConfig: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+        factoryAddress: null, // To fill when contract is deployed
+        wrappedToken: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+        chainId: 1,
     },
     bsc: {
         providerConfig: 'https://bsc-dataseed.binance.org/',
